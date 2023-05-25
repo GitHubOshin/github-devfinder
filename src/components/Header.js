e646b15b1f3cf30dbd3236c9523855910759bea3
OSHIN <izkp44@gmail.com>
2023-05-25 23:33:35 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,19 @@
 function Header() {
-  return <header className="">header</header>
+  return (
+    <header className="w-full">
+      <div className="text-white flex justify-between">
+        <h1>devfinder</h1>
+        <button>LIGHT</button>
+      </div>
+      <form className="bg-dmBgContent flex justify-between">
+        <input
+          className="w-full max-w-[600px] mx-[42px] bg-dmBgContent"
+          placeholder="Search GitHub username..."
+        />
+        <button className="bg-dmSearchBtn text-white">Search</button>
+      </form>
+    </header>
+  )
 }
 
 export default Header
