b52e7d36089b35ffbd38b94c82a24d57aa388135
OSHIN <izkp44@gmail.com>
2023-05-27 00:33:12 +0700

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -16,21 +16,29 @@ function UserProfile(props) {
     company
   } = props
   return (
-    <section className="bg-dmBgContent text-white">
-      <img alt="Profile" src={profile_pic} />
-      <div>
-        <div className="text-white">
-          <b>{name}</b>
-          <h6>@{user_id}</h6>
-          <p>Joined {joined}</p>
+    <section className="bg-dmBgContent text-white max-w-[730px] min-w-[327px] max-h-[463px] flex mobile:flex-col desktop:flex-row justify-between rounded-2xl mobile:px-[24px] desktop:px-[47px] desktop:py-[48px]">
+      <img
+        alt="Profile"
+        src={profile_pic}
+        className="min-w-[70px] max-w-[117px] min-h-[70px] max-h-[117px] rounded-full"
+      />
+      <div className="max-w-[480px] w-full flex flex-col gap-y-[30px]">
+        <div className="text-white flex flex-row justify-between">
+          <div>
+            <b className="text-2xl">{name}</b>
+            <h6 className="text-[#1064ff] mt-1">@{user_id}</h6>
+          </div>
+          <p className="">Joined {joined}</p>
         </div>
-        <p>{bio === null ? 'This profile has no bio' : bio}</p>
-        <div className="bg-dm w-full text-white flex">
+        <p className="text-[#B2B5BF]">
+          {bio === null ? 'This profile has no bio' : bio}
+        </p>
+        <div className="bg-dm w-full text-white flex justify-between min-w-[279px] max-w-[480px] rounded-xl px-[32px] py-[20px]">
           <Activity activity="Repos" count={repos} />
           <Activity activity="Followers" count={followers} />
           <Activity activity="Following" count={following} />
         </div>
-        <div className="text-white">
+        <div className="text-white grid grid-cols-2 gap-5">
           <span>{location}</span>
           <span>{twitter === null ? 'Not avilable' : twitter}</span>
           <span>{blog}</span>
