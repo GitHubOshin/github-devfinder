2279d83f3deb21b3302ed57cc900d81b148a71ab
OSHIN <izkp44@gmail.com>
2023-05-26 15:28:20 +0700

    Get data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import UserProfile from './components/UserProfile'
+
 function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [userInfo, setUserInfo] = useState({})
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  async function getUser() {
+    setIsLoading(true)
+    const data = await axios.request('https://api.github.com/users/octocat')
+    setUserInfo(data.data)
+    setIsLoading(false)
+  }
+
+  if (isLoading === true) {
+    console.log('Loading...')
+  } else {
+    console.log('Finished')
+  }
+
+  console.log(userInfo)
   return (
     <div className="bg-dm h-screen flex flex-col gap-3">
       <Header />
