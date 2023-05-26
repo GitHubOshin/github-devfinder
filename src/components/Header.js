5a519e92a814e93297af9d79e1b8f3e9547e8e45
OSHIN <izkp44@gmail.com>
2023-05-26 20:20:03 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,42 @@
-function Header() {
+import { useEffect, useCallback } from 'react'
+
+function Header(props) {
+  const { searchBox, setSearchBox, handleGetUserInfo } = props
+
+  // useEffect(() => {
+  //   handleGetUserInfo()
+  // }, [handleGetUserInfo])
+
+  // const handleSubmitUsername = useCallback(
+  //   async (e) => {
+  //     e.preventDefault()
+  //     await handleGetUserInfo(searchBox)
+  //   },
+  //   [handleGetUserInfo, searchBox]
+  // )
+
   return (
     <header className="w-full">
       <div className="text-white flex justify-between">
         <h1>devfinder</h1>
         <button>LIGHT</button>
       </div>
-      <form className="bg-dmBgContent flex justify-between">
+      <form
+        className="bg-dmBgContent flex justify-between"
+        // onSubmit={handleSubmitUsername}
+      >
         <input
           className="w-full max-w-[600px] mx-[42px] bg-dmBgContent"
           placeholder="Search GitHub username..."
+          type="text"
+          value={searchBox}
+          onChange={(e) => {
+            setSearchBox(e.target.value)
+          }}
         />
-        <button className="bg-dmSearchBtn text-white">Search</button>
+        <button className="bg-dmSearchBtn text-white" type="submit">
+          Search
+        </button>
       </form>
     </header>
   )
