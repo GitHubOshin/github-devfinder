d95fff3f023df5a11b0e4a2bb61b4f937dab0f72
OSHIN <izkp44@gmail.com>
2023-05-27 12:04:04 +0700

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,17 +16,17 @@ function Header(props) {
   // )
 
   return (
-    <header className="w-full">
+    <header className="w-full flex flex-col gap-8">
       <div className="text-white flex justify-between">
-        <h1>devfinder</h1>
+        <h1 className="font-medium text-2xl">devfinder</h1>
         <button>LIGHT</button>
       </div>
       <form
-        className="bg-dmBgContent flex justify-between"
+        className="bg-dmBgContent flex justify-between items-center max-h-[70px] min-h-[60px] pr-[10px] rounded-xl"
         // onSubmit={handleSubmitUsername}
       >
         <input
-          className="w-full max-w-[600px] mx-[42px] bg-dmBgContent"
+          className="w-full max-w-[600px] mx-[42px] bg-dmBgContent text-white"
           placeholder="Search GitHub username..."
           type="text"
           value={searchBox}
@@ -34,7 +34,10 @@ function Header(props) {
             setSearchBox(e.target.value)
           }}
         />
-        <button className="bg-dmSearchBtn text-white" type="submit">
+        <button
+          className="bg-dmSearchBtn text-white max-w-[107px] max-h-[50px] min-h-[46px] min-w-[83px] rounded-lg font-medium"
+          type="submit"
+        >
           Search
         </button>
       </form>
