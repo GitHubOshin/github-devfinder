c82d64699591ecc0ef4d6057d0153a8a589d7378
OSHIN <izkp44@gmail.com>
2023-05-27 12:04:41 +0700

    CSS

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -16,13 +16,13 @@ function UserProfile(props) {
     company
   } = props
   return (
-    <section className="bg-dmBgContent text-white max-w-[730px] min-w-[327px] max-h-[463px] flex mobile:flex-col desktop:flex-row justify-between rounded-2xl mobile:px-[24px] desktop:px-[47px] desktop:py-[48px]">
+    <section className="bg-dmBgContent text-white flex mobile:flex-col desktop:flex-row items-center justify-between rounded-2xl mobile:px-[24px] desktop:px-[47px] desktop:py-[48px] mobile:py-8">
       <img
         alt="Profile"
         src={profile_pic}
         className="min-w-[70px] max-w-[117px] min-h-[70px] max-h-[117px] rounded-full"
       />
-      <div className="max-w-[480px] w-full flex flex-col gap-y-[30px]">
+      <div className="min-w-[279px] max-w-[480px] w-full flex flex-col gap-y-[30px]">
         <div className="text-white flex flex-row justify-between">
           <div>
             <b className="text-2xl">{name}</b>
@@ -33,7 +33,7 @@ function UserProfile(props) {
         <p className="text-[#B2B5BF]">
           {bio === null ? 'This profile has no bio' : bio}
         </p>
-        <div className="bg-dm w-full text-white flex justify-between min-w-[279px] max-w-[480px] rounded-xl px-[32px] py-[20px]">
+        <div className="bg-dm w-full text-white flex justify-between rounded-xl px-[32px] py-[20px]">
           <Activity activity="Repos" count={repos} />
           <Activity activity="Followers" count={followers} />
           <Activity activity="Following" count={following} />
